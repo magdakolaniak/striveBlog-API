eaff862a85ef40950e7e25547b10727da2fdcfa5
magdakolaniak <magda.kolaniak@gmail.com>
2021-06-01 21:14:10 +0200

    CRUD done

--- src/blogPost/index.js
+++ src/blogPost/index.js
@@ -14,6 +14,21 @@ postsRouter.get('/', async (req, res, next) => {
   }
 });
 
+postsRouter.get('/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const blogPost = await postModel.findById(id);
+    if (blogPost) {
+      res.send(blogPost);
+    } else {
+      next(createError(404, `Post with id: ${req.params.id} not found`));
+    }
+  } catch (error) {
+    console.log(error);
+    next(createError(500, 'An error occurde while getting blogs'));
+  }
+});
+
 postsRouter.post('/', async (req, res, next) => {
   try {
     const newBlogPost = new postModel(req.body);
@@ -29,4 +44,38 @@ postsRouter.post('/', async (req, res, next) => {
     }
   }
 });
+postsRouter.put('/:id', async (req, res, next) => {
+  try {
+    const blogPost = await postModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        runValidators: true,
+        new: true,
+      }
+    );
+    if (blogPost) {
+      res.send(blogPost);
+    } else {
+      next(createError(404, `Post with id: ${req.params.id} not found`));
+    }
+  } catch (error) {
+    console.log(error);
+    next(createError(500, 'An error occured while trying to modify post'));
+  }
+});
+postsRouter.delete('/:id', async (req, res, next) => {
+  try {
+    const blogPost = await postModel.findByIdAndDelete(req.params.id);
+    if (blogPost) {
+      res.status(204).send();
+    } else {
+      next(createError(404, `Post with id: ${req.params.id} was not found`));
+    }
+  } catch (error) {
+    console.log(error);
+    next(createError(500, 'An error occured while trying to delete a post'));
+  }
+});
+
 export default postsRouter;
